982996e1e5ea13bba2d55719e727ae5a724c73a3
Deepesh <deepesh259nitk@gmail.com>
2016-01-25 00:42:49 +0000

    End of Facade Pattern, Library.h and .m files show how single interface is provide to access complex subsystem, here PersistencyManager and HTTPClient, hides the complex logic and exposes as simple API.

--- BlueLibrary/LibraryAPI.h
+++ BlueLibrary/LibraryAPI.h
@@ -6,9 +6,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Album.h"
 
 @interface LibraryAPI : NSObject
 
 + (LibraryAPI*)sharedInstance;
+- (NSArray*)getAlbums;
+- (void)addAlbum:(Album*)album atIndex:(int)index;
+- (void)deleteAlbumAtIndex:(int)index;
 
 @end
--- BlueLibrary/LibraryAPI.m
+++ BlueLibrary/LibraryAPI.m
@@ -6,8 +6,29 @@
 //
 
 #import "LibraryAPI.h"
+#import "PersistencyManager.h"
+#import "HTTPClient.h"
+
+@interface LibraryAPI () {
+    PersistencyManager *persistencyManager;
+    HTTPClient *httpClient;
+    BOOL isOnline;
+}
+
+@end
 
 @implementation LibraryAPI
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        persistencyManager = [[PersistencyManager alloc] init];
+        httpClient = [[HTTPClient alloc] init];
+        isOnline = NO;
+    }
+    return self;
+}
 
 + (LibraryAPI*)sharedInstance
 {
@@ -22,6 +43,31 @@
         _sharedInstance = [[LibraryAPI alloc] init];
     });
     return _sharedInstance;
+}
+
+//real strength of the Facade; when some class outside of your system adds a new album, it doesn’t know — and doesn’t need to know — of the complexity that lies underneath.
+
+- (NSArray*)getAlbums
+{
+    return [persistencyManager getAlbums];
+}
+
+- (void)addAlbum:(Album*)album atIndex:(int)index
+{
+    [persistencyManager addAlbum:album atIndex:index];
+    if (isOnline)
+    {
+        [httpClient postRequest:@"/api/addAlbum" body:[album description]];
+    }
+}
+
+- (void)deleteAlbumAtIndex:(int)index
+{
+    [persistencyManager deleteAlbumAtIndex:index];
+    if (isOnline)
+    {
+        [httpClient postRequest:@"/api/deleteAlbum" body:[@(index) description]];
+    }
 }
 
 @end
